c1bab05eb072e6d21e8577e96903a14c807097a5
Agile <agile@Macbook-Pro.local>
2020-07-14 02:51:06 +0430

    Code clean ups

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
-(function() {
+(function () {
   // Start button
   let startButton = document.querySelector("#js--start");
   // Stop button
   let stopButton = document.querySelector("#js--stop");
   // Reset button
   let resetButton = document.querySelector("#js--reset");
-  // Stopwatch, element which will show the stopwatch
+  // Stopwatch, the element which will show the time
   let stopwatch = document.querySelector("#js--stopwatch");
 
-  // Global values to use for the stopwatch
+  // Global variables to use for the stopwatch
   let centiseconds = 0; // Each centisecond is 1 percent of a second
   let seconds = 0;
   let minutes = 0;
 
-  // Show an element on screen
+  // Show an element on the screen
   function hide(element) {
-    element.classList.add("display-none");
+    element.classList.add("u-display-none");
   }
 
-  // Hide an element on screen
+  // Hide an element on the screen
   function show(element) {
-    element.classList.remove("display-none");
+    element.classList.remove("u-display-none");
   }
 
-  // Toggle disable button
-  function disableButton(element, boolean) {
+  // Toggle disable attribute of an element
+  function disable(element, boolean) {
     if (boolean) {
-      element.classList.add("button-disabled");
       element.disabled = true;
     } else {
-      element.classList.remove("button-disabled");
       element.disabled = false;
     }
   }
 
-  // Format and display the time on screen
+  // Format and display the time on the screen
   function display(centiseconds, seconds, minutes) {
     var formattedCentiseconds = "";
     var formattedSeconds = "";
     var formattedMinutes = "";
 
-    // Format centiseconds before shown on screen
+    // Format centiseconds before shown on the screen
     if (centiseconds < 10) {
       // If centiseconds are less than 10, add a zero before the centiseconds in string
       formattedCentiseconds = "0" + centiseconds.toString();
@@ -49,7 +47,7 @@
       formattedCentiseconds = centiseconds.toString();
     }
 
-    // Format seconds before shown on screen
+    // Format seconds before shown on the screen
     if (seconds < 10) {
       // If seconds are less than 10, add a zero before the seconds in string
       formattedSeconds = "0" + seconds.toString();
@@ -58,7 +56,7 @@
       formattedSeconds = seconds.toString();
     }
 
-    // Format minutes before shown on screen
+    // Format minutes before shown on the screen
     if (minutes < 10) {
       // If minutes are less than 10, add a zero before the minutes in string
       formattedMinutes = "0" + minutes.toString();
@@ -74,27 +72,27 @@
   // Initialize and display the stopwatch
   display(0, 0, 0);
 
-  // When start button clicked
-  // Users will only see this button if the stopwatch is stopped
-  startButton.addEventListener("click", function() {
-    // Show and hide relevent buttons
+  // When start button gets clicked
+  // Users can only see this button while the stopwatch is paused
+  startButton.addEventListener("click", function () {
+    // Change the buttons
     hide(startButton);
-    disableButton(resetButton, true);
     show(stopButton);
+    disable(resetButton, true);
 
-    // Start the stopwatch, this function will every 0.1 seconds
-    let stopwatch = setInterval(function() {
-      // Increment miliseconds by one
+    // Start the stopwatch, this function will run 100 times a second
+    let stopwatch = setInterval(function () {
+      // Increment centiseconds by one
       centiseconds = centiseconds + 1;
 
-      // If miliseconds are equal to 100, reset the centiseconds and increment
+      // If centiseconds are equal to 100, reset the centiseconds and increment
       // seconds by one
       if (centiseconds === 100) {
         centiseconds = 0;
         seconds = seconds + 1;
       }
 
-      // If seconds are equal to 60, reset the seconds and incement minutes by one
+      // If seconds are equal to 60, reset the seconds and increment minutes by one
       if (seconds === 60) {
         seconds = 0;
         minutes = minutes + 1;
@@ -104,27 +102,27 @@
       display(centiseconds, seconds, minutes);
     }, 10);
 
-    // When stop button clicked
-    // Users will only see this button if the stopwatch is running
-    stopButton.addEventListener("click", function() {
-      // Hide and show relevent buttons
+    // When stop button gets clicked
+    // Users can only see this button while the stopwatch is running
+    stopButton.addEventListener("click", function () {
+      // Change the buttons
       hide(stopButton);
       show(startButton);
-      disableButton(resetButton, false);
+      disable(resetButton, false);
 
-      // Stop the stopwatch, the 'stopwatch' value passed on is the setInterval function
+      // Stop the stopwatch, the 'stopwatch' value that gets passed on is the setInterval function
       clearInterval(stopwatch);
     });
   });
 
-  // When reset button clicked
-  // Users will only see this button if the stopwatch has stopped after running
-  resetButton.addEventListener("click", function() {
-    // Hide relevent buttons
-    disableButton(resetButton, true);
+  // When reset button gets clicked
+  // Users can only see this button if the stopwatch stops while running
+  resetButton.addEventListener("click", function () {
+    // Change the buttons
     hide(stopButton);
+    disable(resetButton, true);
 
-    // Reset the time
+    // Reset the global variables
     centiseconds = 0;
     seconds = 0;
     minutes = 0;
